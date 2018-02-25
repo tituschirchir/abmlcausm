62aa5aaf821ca2c56b99fc140ffe5954ec9b9fca
tituschirchir <titus.chirchir12@gmail.com>
2018-02-24 19:55:43 -0500

    Remove dependency on beautiful soup. After resolving issue #18, there shouldn'd be need for data scraping

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import numpy as np
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 from plotly.graph_objs import *
-from network import bank_agent as ba
+
 import helpers.data_downloader as dd
-import helpers.data_scraper as ds
 from data.meta_data import network_layouts, layouts, kawai, network_types
 from data.meta_data import tickers
+from network import bank_agent as ba
 from network.financial_model import FinancialModel
 
 app = dash.Dash()
